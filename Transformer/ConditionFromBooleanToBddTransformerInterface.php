12d2e30632d752782abd33d01f1864d122b031b7
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-02-17 14:33:50 +0100

    change abstract to interface

---
+++ Transformer/ConditionFromBooleanToBddTransformerInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OpenOrchestra\Transformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * Class ConditionFromBooleanToBddTransformer
+ */
+interface ConditionFromBooleanToBddTransformerInterface extends DataTransformerInterface
+{
+
+    /**
+     * @param string $field
+     */
+    public function setField($field);
+}
